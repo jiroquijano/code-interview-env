e446bbcdb9075728debd9944acd204cedad86fb6
safiro.quijano <safiro.quijano@paymaya.com>
2021-09-23 01:58:50 +0800

    added axios sample

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+const axios = require('axios');
+
 const foo = () => {
     return 'pass';
 }
 
+const bar = () => {
+    return {
+        result: 'pass'
+    }
+}
+
+const baz = async () => {
+    const users = await axios.get('https://reqres.in/api/users');
+    return users;
+}
+
 module.exports = {
-    foo
+    foo,
+    bar,
+    baz
 }
--- index.test.js
+++ index.test.js
@@ -1,7 +1,29 @@
-const {foo} = require('./index');
+const {foo, bar, baz} = require('./index');
+const axios = require('axios');
+
+jest.mock('axios');
 
 describe('index', () => {
-    it('should pass', () => {
-        expect(foo()).toBe('pass');
+    
+    describe('foo', () => {
+        it('should return string pass', () => {
+            expect(foo()).toBe('pass');
+        })
+    })
+
+    describe('bar', () => {
+        it('should return object with result', () => {
+            expect(bar()).toEqual({
+                result: 'pass'
+            })
+        })
+    })
+
+    describe('baz', () => {
+        it('should call axios get', async () => {
+            const result = baz();
+            expect(axios.get).toHaveBeenCalledTimes(1);
+            expect(result).not.toBeNull();
+        })
     })
 })
