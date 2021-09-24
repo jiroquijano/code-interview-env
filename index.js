17aa74c21b7569fcd715816524b136608905e988
safiro.quijano <safiro.quijano@paymaya.com>
2021-09-25 04:21:38 +0800

    created fibonacci solution and added test

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ const baz = async () => {
     return users;
 }
 
+const generateFibonacciArray = (numElements) => {
+    let fibonacciArray = [];
+    for(i = 0; i < numElements; i++) {
+        const newElement = fibonacciArray.length <= 1 ? 1 : fibonacciArray[i-2] + fibonacciArray[i-1];
+        fibonacciArray.push(newElement);
+    }
+    return fibonacciArray;
+}
+
 module.exports = {
     foo,
     bar,
-    baz
+    baz,
+    generateFibonacciArray
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {foo, bar, baz} = require('./index');
+const {foo, bar, baz, generateFibonacciArray} = require('./index');
 const axios = require('axios');
 
 jest.mock('axios');
@@ -24,6 +24,24 @@ describe('index', () => {
             const result = baz();
             expect(axios.get).toHaveBeenCalledTimes(1);
             expect(result).not.toBeNull();
+        })
+    })
+
+    describe('generateFibonacciArray', () => {
+        it('should return an array', () => {
+            expect(generateFibonacciArray()).toEqual([]);
+        })
+        it('should return a fibonnacci sequence with 1 element', () => {
+            expect(generateFibonacciArray(1)).toEqual([1]);
+        })
+        it('should return a fibonnacci sequence with 2 elements', () => {
+            expect(generateFibonacciArray(2)).toEqual([1,1]);
+        })
+        it('should return a fibonnacci sequence with 3 elements', () => {
+            expect(generateFibonacciArray(3)).toEqual([1,1,2]);
+        })
+        it('should return a fibonnacci sequence with 8 elements', () => {
+            expect(generateFibonacciArray(8)).toEqual([1,1,2,3,5,8,13,21]);
         })
     })
 })
