59cd95f125f316d9ac259eece1b0132fe8278976
safiro.quijano <safiro.quijano@paymaya.com>
2021-09-25 05:23:38 +0800

    implemented character counting for string

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const foo = () => {
     return 'pass';
 }
 
+const countCharacters = (character, inputString) => {
+    let count = 0
+    inputString.forEach((currentChar)=>{
+        if(currentChar === character) count++;
+    });
+    return count;
+}
+
+const test = (inputString) => {
+    if(typeof inputString !== 'string' || inputString.length == 0) return {};
+    let resultCount = {};
+    const stringArray = inputString.split('');
+    const uniqueCharacters = new Set(stringArray);
+    uniqueCharacters.forEach((character)=>{
+        resultCount[character] = countCharacters(character, stringArray);
+    })
+    return resultCount;
+}
+
 module.exports = {
-    foo
+    foo,
+    test
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {foo, bar, baz} = require('./index');
+const {foo, test} = require('./index');
 
 describe('index', () => {
     
@@ -6,6 +6,31 @@ describe('index', () => {
         it('should return string pass', () => {
             expect(foo()).toBe('pass');
         })
+    })
+
+    //write a function that takes a string and returns count of each character in the string.
+    //input <string> : string whose characters will be counted
+    //output <object> : counts of each character in the string in the following format: {[character]: [count]}
+    describe('test', () => {
+        it('should be able to count a string with only single characters', () => {
+            expect(test('aaa')).toEqual({'a': 3});
+        })
+        it('should be able to count characters in the string with different characters', () => {
+            expect(test('aab')).toEqual({'a': 2, 'b': 1});
+        })
+        it('should be able to count characters in the alphabet', () => {
+            expect(test('alphabet')).toEqual({'a': 2, 'l': 1, 'p': 1, 'h': 1, 'b': 1, 'e': 1, 't': 1});
+        })
+        it('should return empty object when input string is undefined', ()=> {
+            expect(test(undefined)).toEqual({});
+        })
+        it('should return empty object when input string is null', () => {
+            expect(test(null)).toEqual({});
+        })
+        it('should return empty object when input string is empty', () => {
+            expect(test('')).toEqual({});
+        })
+        
     })
 
 })
