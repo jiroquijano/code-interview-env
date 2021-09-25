516abbf8e7cac43050b94b7ba443cb12011b94b7
safiro.quijano <safiro.quijano@paymaya.com>
2021-09-26 04:06:37 +0800

    implemented same array checking

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ const foo = () => {
     return 'pass';
 }
 
+/*  write a function named same, which checks if the values of the 2 arrays are similar
+    only that the second array is squared. frequency of each array elements should be the same.
+
+    //inputs: 2 arrays.
+    //output: a boolean determining if the arrays are 'same'
+*/
+
+//countFrequency() takes in an array and returns an object with number of elements
+// countFrequency([1,1,1]) returns {'1': 3}
+const countFrequency = (array) => {
+    let result = {}
+    for(const arrElement of array) {
+        let elementCount = result[arrElement];
+        if(!elementCount) {
+            result[arrElement] = 1;
+        } else {
+            result[arrElement] = ++elementCount;
+        }
+    }
+    return result;
+}
+
+const same = (array1, array2) => {
+    if(!Array.isArray(array1) || !Array.isArray(array2)) return 'invalid params';
+    if(array1.length !== array2.length) return false;
+    let result = false;
+    const squaredArray1WithFreq = countFrequency(array1.map((number)=>number**2));
+    const array2WithFreq = countFrequency(array2);
+    for(const objElem of Object.keys(squaredArray1WithFreq)) {
+        result = (array2WithFreq[objElem] === squaredArray1WithFreq[objElem]) ? true : false
+    }
+    return result;
+}
+
 module.exports = {
-    foo
+    foo,
+    same
 }
--- index.test.js
+++ index.test.js
@@ -1,10 +1,46 @@
-const {foo, bar, baz} = require('./index');
+const {foo, same} = require('./index');
 
 describe('index', () => {
     
     describe('foo', () => {
         it('should return string pass', () => {
             expect(foo()).toBe('pass');
+        })
+    })
+
+    /*  write a function named same, which checks if the values of the 2 arrays are similar
+    only that the second array is squared. frequency of each array elements should be the same.
+
+    //inputs: 2 arrays.
+    //output: a boolean determining if the arrays are 'same'
+*/
+    describe('same', () => {
+        it('should return true if arrays are the same', () => {
+            expect(same([1,1,1], [1,1,1])).toBe(true);
+        });
+        it('should return false if the array lengths are not equal', () => {
+            expect(same([1],[1,1])).toBe(false);
+        });
+        it('should return true if 2nd array has values which is squared of values in array 1', () => {
+            expect(same([2],[4])).toBe(true);
+        });
+        it('should return true if 2nd array contains elements which are squared of array 1 values', () => {
+            expect(same([1,2],[1,4])).toBe(true);
+        })
+        it('should check same-ness regardless of order', () => {
+            expect(same([3,2],[4,9])).toBe(true);
+        })
+        it('should return false if frequency of the squared value is not same', () => {
+            expect(same([1,1,3], [9,9,9])).toBe(false);
+        })
+        it('should return true if the squared values and frequency are the same', () => {
+            expect(same([1,3,4],[9,16,1])).toBe(true);
+        });
+        it('should gracefully handle if 1st argument is not an array', () => {
+            expect(same(undefined,[1])).toBe('invalid params');
+        })
+        it('should gracefully handle if 2nd argument is not an array', () => {
+            expect(same([1],null)).toBe('invalid params');
         })
     })
 
