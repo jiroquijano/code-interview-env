b558dc05a6f2ad81cbe60ffc30e1c6cd3ab2144b
safiro.quijano <safiro.quijano@paymaya.com>
2021-09-26 19:18:21 +0800

    implemented valid anagram checking

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ const foo = () => {
     return 'pass';
 }
 
+//accepts a string and returns an object with the format: {'a': <number of a's>}
+const countCharacters = (string) => {
+    let result = {}
+    for(const character of string) {
+        if(!result[character]) {
+            result[character] = 1;
+        } else {
+            result[character] += 1;
+        }
+    }
+    return result;
+}
+
+/*
+    Given 2 strings, check if the second string is an anagram of the first.
+    //inputs: 2 strings. 1st string is the word, 2nd string is the jumbled letters
+    //output: boolean. true if the 2nd string is an anagram for the first string.
+*/
+
+const isValidAnagram = (string1, string2) => {
+    if(string1.length !== string2.length) return false;
+    //create a result variable. default to false
+    let result = false;
+    //compare the strings. string2 should have letters in string 1 with the same frequency
+    //create an object which lists the frequency of each letters for both string 1 and string 2
+    const string1CharCount = countCharacters(string1.toLowerCase());
+    const string2CharCount = countCharacters(string2.toLowerCase());
+    //check if the characters in string 2 is present in string 1 and have the same count
+    result = Object.keys(string2CharCount).every((character)=>{
+        return (string1CharCount[character] && string1CharCount[character] === string2CharCount[character]);
+    })
+    return result;
+}
+
 module.exports = {
-    foo
+    foo,
+    isValidAnagram
 }
--- index.test.js
+++ index.test.js
@@ -1,10 +1,25 @@
-const {foo, bar, baz} = require('./index');
+const {foo, isValidAnagram} = require('./index');
 
 describe('index', () => {
     
     describe('foo', () => {
         it('should return string pass', () => {
             expect(foo()).toBe('pass');
+        })
+    })
+
+    describe('isValidAnagram', () => {
+        it('should return false if the string lengths do not match', () => {
+            expect(isValidAnagram('aaa', 'aaaa')).toBe(false);
+        })
+        it('should return true if the second string is a valid anagram', () => {
+            expect(isValidAnagram('vallid', 'dillav')).toBe(true);
+        });
+        it('should return false if string2 is not a valid anagram', () => {
+            expect(isValidAnagram('valid','notvalid')).toBe(false);
+        })
+        it('should not be case sensitive', ()=> {
+            expect(isValidAnagram('valid', 'dVali')).toBe(true);
         })
     })
 
