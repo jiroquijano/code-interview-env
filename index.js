e0592ab28a24a9de0a49261b2c7edeca25bedc7c
safiro.quijano <safiro.quijano@paymaya.com>
2021-09-26 20:02:58 +0800

    implemented sum zero using multiple pointer pattern

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ const foo = () => {
     return 'pass';
 }
 
+/*
+    write a function which finds a pair of integers whose sum is zero
+    input: an array of numbers
+    output: an array containing the pair of numbers whose sum is zero
+*/
+
+const sumZero = (inputArray) => {
+    //sort the inputArray
+    const sortedArray = inputArray.sort((a,b)=>a-b);
+    //create two pointers in the array (beginning and end)
+    let beginIndex = 0;
+    let endIndex = sortedArray.length - 1;
+    //add the value of the two pointers.
+    //  if sum is zero, return the value of the pointers.
+    //  if sum is positive, move the end pointer to the left, else move beginning pointer to the right
+    while (beginIndex < endIndex) {
+        const leftValue = sortedArray[beginIndex];
+        const rightValue = sortedArray[endIndex];
+        const sum = leftValue + rightValue;
+        if(sum === 0) {
+            return [leftValue, rightValue];
+        } else if(sum > 0) {
+            --endIndex;
+        } else {
+            ++beginIndex;
+        }
+    }
+}
+
 module.exports = {
-    foo
+    foo,
+    sumZero
 }
--- index.test.js
+++ index.test.js
@@ -1,10 +1,28 @@
-const {foo, bar, baz} = require('./index');
+const {foo, sumZero} = require('./index');
 
 describe('index', () => {
     
     describe('foo', () => {
         it('should return string pass', () => {
             expect(foo()).toBe('pass');
+        })
+    })
+
+    describe('sumZero', () => {
+        it('should return a pair of numbers whose sum is zero', ()=>{
+            expect(sumZero([-1,1])).toEqual([-1,1])
+        })
+        it('should return a pair of numbers whose sum is zero', ()=>{
+            expect(sumZero([-1,0,1,2])).toEqual([-1,1])
+        })
+        it('should return a pair of numbers whose sum is zero', ()=>{
+            expect(sumZero([-2,0,1,2,3,5])).toEqual([-2,2])
+        })
+        it('should return undefined if no pair sums to zero', ()=>{
+            expect(sumZero([-2,0,1])).toEqual(undefined)
+        })
+        it('should return undefined if no pair sums to zero', () => {
+            expect(sumZero([6,2,1,5])).toBe(undefined);
         })
     })
 
