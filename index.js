86f8b8262534e772f6a32356e5a4c4909b837244
safiro.quijano <safiro.quijano@paymaya.com>
2021-10-07 20:31:25 +0800

    implemented count unique numbers

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ const foo = () => {
     return 'pass';
 }
 
+/*create a function which accepts an array and counts the number of
+unique numbers. there can be negative values in the array.
+input: array of numbers (negative, positive, zero)
+output: number of unique elements of the array.
+*/
+//[-1,0,1,2,3] => 5
+const countUnique = (inputArray) => {
+    //sort the input array
+    const sortedArray = inputArray.sort((a,b)=>a-b);
+    //create two pointers, (tail[starts at index 0], current[ head at index 1])
+    let tail = 0, head = 1;
+    //when head is greater than or equal to array length, stop
+    while(head < sortedArray.length) {
+        //compare the values of tail and head pointer
+        if(sortedArray[tail] === sortedArray[head]) { 
+            //if values are equal, splice out the element on head's index
+            sortedArray.splice(head,1)
+        } else { //if values are not equal, increment tail and head by 1
+            tail += 1;
+            head += 1;
+        }
+    }
+    //return length of sortedArray
+    return sortedArray.length;
+}
+
 module.exports = {
-    foo
+    foo,
+    countUnique
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {foo, bar, baz} = require('./index');
+const {foo, countUnique} = require('./index');
 
 describe('index', () => {
     
@@ -6,6 +6,18 @@ describe('index', () => {
         it('should return string pass', () => {
             expect(foo()).toBe('pass');
         })
+    })
+
+    describe('countUnique', () => {
+        it('should be able to count elements on an array with only one unique number', () => {
+            expect(countUnique([1,1,1,1])).toBe(1);
+        });
+        it('should be able to count elements on an array with multiple unique numbers', () => {
+            expect(countUnique([1,1,2,3])).toBe(3);
+        });
+        it('should be able to count elements on an array with negative, positive and zero values', () => {
+            expect(countUnique([-1,0,1,2,3])).toBe(5);
+        });
     })
 
 })
