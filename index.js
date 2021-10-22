9d54f7a2e3bdbb079a9f39767309fe2b2571b8be
safiro.quijano <safiro.quijano@paymaya.com>
2021-10-23 02:22:35 +0800

    save nested interview

--- index.js
+++ index.js
@@ -2,6 +2,43 @@ const foo = () => {
     return 'pass';
 }
 
+
+    /*
+    Write a function that takes a string as an argument and generates an array that sums all the repeating characters.
+
+    Test Cases
+    compress(‘’) // []
+    compress(‘abcd’) // [[1, ‘a’], [1, ‘b’], [1, ‘c’], [1, ‘d’]]
+    compress(‘aaaaaaaabaaaa’) // [[8,’a’], [1,’b’], [4,’a’]]
+    */
+
+    //input: string
+    //output: array, with key[0]: frequency value[1]: letter
+
+
+const frequencyCounter = (inputString) => {
+    const counter = {};
+    //handle the empty string case
+    if(inputString.length === 0) return [];
+    //got through all of the characters in the inputString
+    for(let char of inputString) {
+        //check if the current character on iteration exists in the counter
+        //if it doesn't exist, then set it to 1
+        if(!counter[char]) {
+            counter[char] = 1;
+        } else { //if it exists in the counter, increment
+            counter[char] += 1;
+        }
+    }
+    const result = [];
+    for(const iteration in counter) {
+        const entry = [counter[iteration], iteration]
+        result.push(entry);
+    }
+    return result;
+}
+
 module.exports = {
-    foo
+    foo,
+    frequencyCounter
 }
--- index.test.js
+++ index.test.js
@@ -1,11 +1,36 @@
-const {foo, bar, baz} = require('./index');
+const {foo, frequencyCounter} = require('./index');
 
 describe('index', () => {
-    
-    describe('foo', () => {
-        it('should return string pass', () => {
-            expect(foo()).toBe('pass');
+
+      /*
+    Write a function that takes a string as an argument and generates an array that su
+    ms all the repeating characters.
+
+    Test Cases
+    compress(‘’) // []
+    compress(‘abcd’) // [[1, ‘a’], [1, ‘b’], [1, ‘c’], [1, ‘d’]]
+    compress(‘aaaaaaaabaaaa’) // [[8,’a’], [1,’b’], [4,’a’]]
+    */
+
+    //input: string
+    //output: array, with key[0]: frequency value[1]: letter
+
+    describe('frequencyCounter', () => {
+
+        it('should be able to count a monocharacter string', () => {
+            expect(frequencyCounter('aaaa')).toEqual([[4,'a']]);
+        });
+
+        it('should be able to count a string with multiple unique characters', () => {
+            expect(frequencyCounter('abcd')).toEqual([[1,'a'],[1,'b'],[1,'c'],[1, 'd']]);
+        })
+
+        it('should be able to count a multicharacter string with varied frequency', () => {
+            expect(frequencyCounter('aaaabbc')).toEqual([[4,'a'],[2,'b'],[1,'c']]);
+        })
+        
+        it('should return an empty array if the string is empty', () => {
+            expect(frequencyCounter('')).toEqual([]);
         })
     })
-
 })
